65e012bb8fab78034f1b2b2ea9b1aa93ca63c2e6
aditya Rekhate <111756698+adityarekhate123@users.noreply.github.com>
2023-01-05 15:41:54 +0530

    STRING FUNCTION

---
+++ Strings function.sql
@@ -0,0 +1,11 @@
+------STRING FUNCTION
+
+select 'my name is aditya rekhate'
+select upper('my name is aditya rekhate');
+select 'HEY EVERYONE'
+select lower('HEY EVERYONE')
+select reverse('HEY EVERYONE')
+select ('     aditya')
+select ltrim('     aditya')
+select ('this is mysql workbench')
+select substring('this is mysql workbench',5,11)

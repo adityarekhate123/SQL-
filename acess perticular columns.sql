0dbc96eeae4b8801591104d10442a35bc496b350
aditya Rekhate <111756698+adityarekhate123@users.noreply.github.com>
2023-01-06 09:08:17 +0530

    Add files via upload

---
+++ acess perticular columns.sql
@@ -0,0 +1,5 @@
+select e_name,e_salary from employee;
+select e_name,e_salary from organization;
+select e_department,e_salary from employee;
+select * from employee where e_age>40;
+select * from organization where e_salary>48000;
